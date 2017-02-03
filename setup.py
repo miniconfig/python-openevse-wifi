0d91d3fea6796803967a99e323e3a42f1fdf42b2
miniconfig <dev@miniconfig.com>
2017-02-02 20:48:32 -0500

    Fixed PyPi requires

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 setup(
   name = 'openevsewifi',
   packages = ['openevsewifi'],
-  version = '0.3',
+  version = '0.4',
   description = 'A python library for communicating with the ESP8266-based wifi module from OpenEVSE',
   author = 'Michelle Avery',
   author_email = 'dev@miniconfig.com',
   url = 'https://github.com/miniconfig/python-openevse-wifi',
-  download_url = 'https://github.com/miniconfig/python-openevse-wifi/tarball/0.3',
-  install_requires='urllib'
+  download_url = 'https://github.com/miniconfig/python-openevse-wifi/tarball/0.4'
 )
