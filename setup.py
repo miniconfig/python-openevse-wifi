a5e05d891407bc49c7db2fa6e35875415a9cd078
miniconfig <dev@miniconfig.com>
2017-02-02 19:53:48 -0500

    Renamed openevsewifi.py to __init__.py to make it behave better with pylint.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 setup(
   name = 'openevsewifi',
   packages = ['openevsewifi'],
-  version = '0.1',
+  version = '0.3',
   description = 'A python library for communicating with the ESP8266-based wifi module from OpenEVSE',
   author = 'Michelle Avery',
   author_email = 'dev@miniconfig.com',
   url = 'https://github.com/miniconfig/python-openevse-wifi',
-  download_url = 'https://github.com/miniconfig/python-openevse-wifi/tarball/0.1',
+  download_url = 'https://github.com/miniconfig/python-openevse-wifi/tarball/0.3',
   install_requires='urllib'
 )
