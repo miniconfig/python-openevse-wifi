a0d0fb2145cf4023c1b2a4d4969559d7d4769a61
Diane Trout <diane@ghic.org>
2018-10-28 21:24:19 -0700

    Store content in a variable
    This maked debugging the regex easier with pdb

--- openevsewifi/__init__.py
+++ openevsewifi/__init__.py
@@ -33,9 +33,10 @@ class Charger:
     data = { 'rapi' : command }
     full_url = self.url + urllib.parse.urlencode(data)
     data = urllib.request.urlopen(full_url)
-    response = re.search('\<p>&gt;\$(.+)\<script', data.read().decode('utf-8'))
+    content = data.read().decode('utf-8')
+    response = re.search('\<p>&gt;\$(.+)<script', content)
     if response == None:#If we are using version 1 - https://github.com/OpenEVSE/ESP8266_WiFi_v1.x/blob/master/OpenEVSE_RAPI_WiFi_ESP8266.ino#L357
-      response = re.search('\>\>\$(.+)\<p>', data.read().decode('utf-8'))
+      response = re.search('\>\>\$(.+)\<p>', content)
     return response.group(1).split()
 
   def getStatus(self):
